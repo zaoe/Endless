11162c7eec8df952745a64d26585ba5a3dfd24c5
yuoft <2685245558@qq.com>
2021-11-21 22:14:23 +0800

    修复方块挖掘等级问题

--- src/main/java/com/yuo/endless/Event/EventHandler.java
+++ src/main/java/com/yuo/endless/Event/EventHandler.java
@@ -4,7 +4,6 @@ import com.yuo.endless.Armor.InfinityArmor;
 import com.yuo.endless.Endless;
 import com.yuo.endless.Items.ItemRegistry;
 import com.yuo.endless.Items.Tool.*;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -21,7 +20,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.item.ItemEvent;
@@ -31,7 +29,10 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 事件处理类
@@ -216,10 +217,6 @@ public class EventHandler {
                 world.setBlockState(pos, Blocks.AIR.getDefaultState());
             }
         }
-        if (stack.getItem() == ItemRegistry.infinityIngot.get()){
-            BlockState state = world.getBlockState(pos);
-            player.sendMessage(new TranslationTextComponent("方块信息：" + state.getHarvestLevel()), UUID.randomUUID());
-        }
     }
 
     @SubscribeEvent
@@ -243,18 +240,6 @@ public class EventHandler {
                     event.setNewSpeed(event.getNewSpeed() * 2);
                 }
             }
-        }
-    }
-    @SubscribeEvent
-    public static void hav(PlayerEvent.HarvestCheck event){
-        LivingEntity living = event.getEntityLiving();
-        if (living instanceof PlayerEntity){
-            PlayerEntity player = (PlayerEntity) living;
-            ItemStack heldItem = player.getHeldItem(Hand.MAIN_HAND);
-            BlockState targetBlock = event.getTargetBlock();
-            boolean b = event.canHarvest();
-            player.sendMessage(new TranslationTextComponent(heldItem.getDisplayName().getString() + " : "+
-                    targetBlock.getBlock().getTranslatedName().getString() + " : " + b), UUID.randomUUID());
         }
     }
 
